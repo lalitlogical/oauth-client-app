14b02144058edc0964c8057f5a80e48ded532d7d
Lalit Kumar Maurya <lalit.logical@gmail.com>
2025-06-02 12:02:15 +0530

    Updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_31_181536) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "openid"
+    t.string "name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
